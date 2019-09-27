96b5c1f891c1446fd352794cb0c2014722550c64
Kevin Dam <kevindam@Kevins-MacBook-Pro.local>
2019-09-27 14:09:40 -0700

    Finish activerecord_lite project and add CSS_flex_exercise

--- W4D4/activerecord_lite/lib/03_associatable.rb
+++ W4D4/activerecord_lite/lib/03_associatable.rb
@@ -10,41 +10,80 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
+  attr_reader :options
+
   def initialize(name, options = {})
-    # ...
+    @options = {
+      class_name: name.to_s.singularize.camelcase(:upper),
+      primary_key: :id,
+      foreign_key: ("#{name}_id").to_sym }
+
+    options.each do |key, val|
+      @options[key] = val
+    end
+
+    @options.each do |k, v|
+      self.send("#{k}=", v)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @options = {
+    class_name: name.to_s.singularize.camelcase(:upper),
+    primary_key: :id,
+    foreign_key: ("#{self_class_name.downcase}_id").to_sym
+    }
+
+    options.each do |key, val|
+      @options[key] = val
+    end
+
+    @options.each do |k, v|
+      self.send("#{k}=", v)
+    end
   end
 end
 
 module Associatable
+  attr_accessor :assoc_options
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    belongs = self.assoc_options[name]
+
+    define_method(name) do
+      foreign_key = self.send(belongs.foreign_key)
+      var = belongs.model_class.where(belongs.primary_key => foreign_key)
+      var.first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+    many = self.assoc_options[name]
+
+    define_method(name) do
+      primary_key = self.send(many.primary_key)
+      var = many.model_class.where(many.foreign_key => primary_key)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- W4D4/activerecord_lite/lib/04_associatable2.rb
+++ W4D4/activerecord_lite/lib/04_associatable2.rb
@@ -1,10 +1,29 @@
 require_relative '03_associatable'
+require 'byebug'
 
 # Phase IV
 module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do 
+      through_opts = self.class.assoc_options[through_name]
+      source_opts = through_opts.model_class.assoc_options[source_name]
+
+      data = DBConnection.execute(<<-SQL)
+      SELECT
+        #{source_opts.table_name}.*
+      FROM
+        #{through_opts.table_name}
+      JOIN
+        #{source_opts.table_name} 
+      ON 
+        #{source_opts.table_name}.#{source_opts.primary_key} = #{through_opts.table_name}.#{source_opts.foreign_key}
+      WHERE
+        #{through_opts.table_name}.#{source_opts.foreign_key} = #{source_opts.table_name}.#{source_opts.primary_key}
+      SQL
+
+      source_opts.model_class.parse_all(data).first
+    end
   end
 end
